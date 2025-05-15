166c8d1237919e86ff73e7d1eb847cf5ed0ed523
Elad Rabinovitch <eladrabinovitch@MacBook-Pro-sl-Elad.local>
2025-05-15 11:54:28 +0300

    stracture finished

--- server/src/db.js
+++ server/src/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./db');
-const dotenv = require('dotenv');
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from './db.js';
+import dotenv from 'dotenv';
+import AuthRouter from './routes/authRoutes.js';
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -12,6 +13,8 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Hello from backend!');
 });
+
+app.use('/api/auth', AuthRouter);
 
 const PORT = process.env.PORT || 3000;
 
