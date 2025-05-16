73fdd6457814c4b8d5d9e53a318ce1a35cc35a33
EladRab1106 <eladrabinovitch1106@gmail.com>
2025-05-17 02:52:25 +0300

    Fix socket connection to use VITE_SOCKET_URL only in production

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,9 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = import.meta.env.PROD
+  ? io(import.meta.env.VITE_SOCKET_URL, {
+      transports: ['websocket'],
+    })
+  : null; // או false, או לא לעשות כלום בלוקאלי
 
 export default socket;
